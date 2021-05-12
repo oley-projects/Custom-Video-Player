5a9181712894d35924a9231cd21ae03fd83ea26c
oley-projects <oley.for.git@gmail.com>
2021-05-12 22:49:49 +0300

    Player buttons and progressbar functionality

--- js/script.js
+++ js/script.js
@@ -15,26 +15,39 @@ function toggleVideoStatus() {
 
 // Update play, pause icon
 function updatePlayIcon() {
-    if(video.pause) {
-
+    if(video.paused) {
+        play.classList.add('paused');
     } else {
-        
+        play.classList.remove('paused');
     }
 }
 
 // Update progress and timestamp
 function updateProgress() {
-    return true;
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = `0${mins}`;
+    }
+
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = `0${secs}`;
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
 function stopVideo() {
-    return true;
+    video.currentTime = 0;
+    video.pause();
 }
 
 // Event listeners
